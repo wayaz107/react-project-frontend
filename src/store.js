c85f3b36d25da1e5554944602a08ff7184f11a99
Wardayaz <wayaz107@gmail.com>
2021-04-24 02:46:08 -0500

    added rcombine reducer in store.js

--- src/store.js
+++ src/store.js
@@ -1,9 +1,14 @@
 import {createStore,applyMiddleware,compose} from 'redux';
-import reducers from './reducers'
+import productsReducer from './reducers/productsReducer.js'
+import commentsReducer from './reducers/commentsReducer.js'
 import thunk from 'redux-thunk';
 
+const reducer = combineReducers({
+    products: productsReducer,
+    commments: commentsReducer
+})
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducers, composeEnhancer(applyMiddleware(thunk)))
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 
 export default store
