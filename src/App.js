ef31042c1ec7764f1eb6c433e5e610dbf9be6485
Wardayaz <wayaz107@gmail.com>
2021-04-24 02:40:35 -0500

    added reducers file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 class App extends React.Component {
 
 componentDidMount() {
-  fetch("http://localhost:3000/recipes") 
+  fetch("http://localhost:3000/api/v1/products",{
+    method: 'GET'
+  }) 
   .then(response => response.json())
   .then(data => console.log(data))
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore,applyMiddleware,compose} from 'redux';
 import {Provider} from 'react-redux';
-import thunk from 'redux-thunk';
 // import './index.css';
 import App from './App';
+import store from './store.js'
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 
 
 ReactDOM.render(
@@ -17,8 +14,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
