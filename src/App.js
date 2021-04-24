eabdabb1643eb30ce16e84066f3258309e3186ee
Wardayaz <wayaz107@gmail.com>
2021-04-23 22:19:23 -0500

    updated package.json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 class App extends React.Component {
+
+componentDidMount() {
+  fetch("http://localhost:3000/recipes") 
+  .then(response => response.json())
+  .then(data => console.log(data))
+};
+
+
   render(){
     return (
       <div className="App">
