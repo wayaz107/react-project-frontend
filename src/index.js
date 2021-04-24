88035455f8ac37cc4f502c6dbfb5659531f12cb8
Wardayaz <wayaz107@gmail.com>
2021-04-23 22:05:52 -0500

    updated app and index file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {createStore,applyMiddleware,compose} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+// import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
