65086f22655a8ab42c2909057439742f77fa0842
Wardayaz <wayaz107@gmail.com>
2021-04-25 03:13:21 -0500

    added Home component

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Home = () => {
+    return(
+        <div className="Home">
+            <h1>Diva Skin Lounge</h1>
+             <Link to="/products">
+                <Button>
+                 Skincare Products
+                 </Button>
+            </Link>
+        </div>
+    )
+}
