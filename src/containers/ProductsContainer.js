90be451c8e472714a4268a7c648782faecce8ed9
Wardayaz <wayaz107@gmail.com>
2021-04-25 03:31:02 -0500

    added productsContainer

---
+++ src/containers/ProductsContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ProductsContainer extends React.Component{
+    render() {
+        return (
+            <div>
+                ProductsContainer
+            </div>
+        )
+    }
+}
+
+export default ProductsContainer;
+
