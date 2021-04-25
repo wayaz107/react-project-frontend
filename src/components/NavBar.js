98a0bd384935bcee8e79fd850c1ad38d64c60858
Wardayaz <wayaz107@gmail.com>
2021-04-25 03:26:58 -0500

    added navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,5 +2,13 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 
 const NavBar = () => {
-    
+    return(
+        <div>
+            <NavLink>Diva Skin Lounge</NavLink>
+            <NavLink>Home</NavLink>
+            <NavLink>SkinCare Products</NavLink>
+        </div>
+    )
 }
+
+export default NavBar;
