b9cb89c9b450513cb261b1fc2424e33470365d0b
Wardayaz <wayaz107@gmail.com>
2021-04-26 04:58:45 -0500

    added product form

--- src/components/ProductInput.js
+++ src/components/ProductInput.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import { Button, Form, Input } from 'reactstrap';
+import { Button, Form, Input,Label} from 'reactstrap';
 
 class ProductInput extends React.Component {
 
@@ -10,15 +10,36 @@ class ProductInput extends React.Component {
         image:''
     };
 
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
+
+handleSubmit = e => {
+    e.preventDefault();
+    
+
+  
+}
+
+
+
     render(){
-        console.log(this.props)
         return(
             <div className='ProductFormContainer'>
-                <Form className='ProductForm'>
+                <Form className='ProductForm' onSubmit={this.handleSubmit}>
                     <h5>Add a new Skincare Product</h5>
-                    <input type='text' name='name' value={this.state.name} />
+                    <Label>Name</Label>
+                    <input type='text' name='name' value={this.state.name} placeholder='Product Name' onChange={this.handleChange}/>
+                     <br></br>
+                    <Label>Brand</Label>
+                    <input type='text' name='brand' value={this.state.brand} placeholder='Product Brand' onChange={this.handleChange}/>
+                    <br></br>
+                    <Label>Image</Label>
+                    <input type='text' name='image' value={this.state.image} placeholder='Product Image' onChange={this.handleChange}/>
+                <Button>Add Product</Button>
                 </Form>
-
             </div>
         )
     }
