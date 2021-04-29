fd5d7925656ca2873ef157c279612ac503663942
Wardayaz <wayaz107@gmail.com>
2021-04-29 02:43:51 -0500

    added product show route to the main container

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -4,7 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import {fetchProducts} from '../actions/productActions';
 import ProductInput from '../components/ProductInput';
 import ProductsList from '../components/ProductsList';
-// import ProductShow from '../components/ProductShow';
+import ProductShow from '../components/ProductShow';
 import OwnedProducts from '../components/OwnedProducts';
 import WishList from '../components/WishList';
 
@@ -21,8 +21,8 @@ class ProductsContainer extends React.Component{
                 <Route path='/products/new' component={ProductInput} />
                 <Route path='/products/owned' render={(routerProps) => <OwnedProducts {...routerProps} products={this.props.products}/>}/>
                 <Route path='/products/wish-list' render={(routerProps) => <WishList {...routerProps} products={this.props.products}/>}/>
+                <Route path='/products/:id' render={(routerProps) => <ProductShow {...routerProps} products={this.props.products}/>} />
                 <Route path='/products' render={(routerProps) => < ProductsList {...routerProps} products={this.props.products}/>}/>
-                {/* <ProductShow products={this.props.products}/> */}
                 </Switch>
             </div>
         )
